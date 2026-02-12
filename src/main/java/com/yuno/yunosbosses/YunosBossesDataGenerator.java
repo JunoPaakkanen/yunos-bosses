aebc241af54dea0bc59d9c5c97c78492ae773e94
Juno <juno.paakkanen@gmail.com>
2026-02-12 18:56:17 +0200

    Initial commit

---
+++ src/main/java/com/yuno/yunosbosses/YunosBossesDataGenerator.java
@@ -0,0 +1,11 @@
+package com.yuno.yunosbosses;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class YunosBossesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
