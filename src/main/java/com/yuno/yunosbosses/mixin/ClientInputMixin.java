062bf4a6e7cb8f6502839d2468da72ad25b70095
Juno <juno.paakkanen@gmail.com>
2026-09-01 02:39:11 +0300

    Update ClientInputMixin for 1.21.7
    - In newer versions tick() in KeyboardInput only takes CallbackInfo ci.
    - Setting playerInput to DEFAULT resets all inputs.
    - Setting movementVector to ZERO cancels all movement.
    - Made the Mixin class abstract and extend Input so it can cleanly assign this.playerInput and this.movementVector.

--- src/main/java/com/yuno/yunosbosses/mixin/ClientInputMixin.java
+++ src/main/java/com/yuno/yunosbosses/mixin/ClientInputMixin.java
@@ -4,29 +4,22 @@ import com.yuno.yunosbosses.effect.ModEffects;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.input.Input;
 import net.minecraft.client.input.KeyboardInput;
+import net.minecraft.util.PlayerInput;
+import net.minecraft.util.math.Vec2f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(KeyboardInput.class)
-public class ClientInputMixin {
+public abstract class ClientInputMixin extends Input {
 
     @Inject(method = "tick", at = @At("TAIL"))
-    private void yunosbosses$paralyzeMovementInput(boolean slowDown, float slowDownFactor, CallbackInfo ci) {
+    private void yunosbosses$paralyzeMovementInput(CallbackInfo ci) {
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.player != null && client.player.hasStatusEffect(ModEffects.FRAME_FREEZE)) {
-            Input input = (Input) (Object) this;
-
-            // Override and zero out all active keyboard input values
-            input.movementSideways = 0.0F;
-            input.movementForward = 0.0F;
-            input.pressingLeft = false;
-            input.pressingRight = false;
-            input.pressingForward = false;
-            input.pressingBack = false;
-            input.jumping = false;
-            input.sneaking = false;
+            this.playerInput = PlayerInput.DEFAULT;
+            this.movementVector = Vec2f.ZERO;
         }
     }
 }
