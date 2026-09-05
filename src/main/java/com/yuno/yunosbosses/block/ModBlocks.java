5c6bd25c8af297b4282357c5039fc83eb5db2afa
Juno <juno.paakkanen@gmail.com>
2026-09-05 04:31:32 +0300

    Update ModBlocks and ModItems for 1.21.7
    - Migrated to using RegistryKeys for custom items and blocks.

--- src/main/java/com/yuno/yunosbosses/block/ModBlocks.java
+++ src/main/java/com/yuno/yunosbosses/block/ModBlocks.java
@@ -7,23 +7,35 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
+
 public class ModBlocks {
 
-    public static final Block DOMAIN_FLOOR = registerBlock("domain_floor", new DomainFloorBlock(AbstractBlock.Settings.create()
+    public static final Block DOMAIN_FLOOR = registerBlock("domain_floor", DomainFloorBlock::new, AbstractBlock.Settings.create()
             .strength(-1.0f, 3600000.0f)
             .dropsNothing()
             .nonOpaque()
             .luminance(state -> 9)
             .sounds(BlockSoundGroup.MUD)
-    ));
+    );
 
-    private static Block registerBlock(String name, Block block) {
-        Identifier id = Identifier.of("yunosbosses", name);
-        Registry.register(Registries.ITEM, id, new BlockItem(block, new Item.Settings()));
-        return Registry.register(Registries.BLOCK, id, block);
+    private static Block registerBlock(String name, Function<AbstractBlock.Settings, Block> blockFactory, AbstractBlock.Settings settings) {
+        Identifier id = Identifier.of(YunosBosses.MOD_ID, name);
+        RegistryKey<Block> blockKey = RegistryKey.of(RegistryKeys.BLOCK, id);
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, id);
+
+        Block block = blockFactory.apply(settings.registryKey(blockKey));
+        Registry.register(Registries.BLOCK, blockKey, block);
+
+        BlockItem blockItem = new BlockItem(block, new Item.Settings().registryKey(itemKey).useBlockPrefixedTranslationKey());
+        Registry.register(Registries.ITEM, itemKey, blockItem);
+
+        return block;
     }
 
     public static void registerModBlocks() {
--- src/main/java/com/yuno/yunosbosses/item/ModItems.java
+++ src/main/java/com/yuno/yunosbosses/item/ModItems.java
@@ -9,23 +9,30 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
 
 public class ModItems {
     // Components
     public static final FoodComponent STRANGE_FRUIT_COMPONENT = new FoodComponent.Builder().nutrition(4).saturationModifier(0.3f).alwaysEdible().build();
 
     // Items
-    public static final Item STRANGE_FRUIT = registerItem("strange_fruit", new StrangeFruitItem(new Item.Settings().food(STRANGE_FRUIT_COMPONENT)));
+    public static final Item STRANGE_FRUIT = registerItem("strange_fruit", StrangeFruitItem::new, new Item.Settings().food(STRANGE_FRUIT_COMPONENT));
 
     // Staff Items
-    public static final Item BASIC_MAGICAL_STAFF = registerItem("basic_magical_staff", new StaffItem(new Item.Settings().maxCount(1), 1));
-    public static final Item UBEL_STAFF = registerItem("ubel_staff", new StaffItem(new Item.Settings().maxCount(1), 1.5F));
+    public static final Item BASIC_MAGICAL_STAFF = registerItem("basic_magical_staff", settings -> new StaffItem(settings, 1), new Item.Settings().maxCount(1));
+    public static final Item UBEL_STAFF = registerItem("ubel_staff", settings -> new StaffItem(settings, 1.5F), new Item.Settings().maxCount(1));
 
     // Helper method to register item
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(YunosBosses.MOD_ID, name), item);
+    private static <T extends Item> T registerItem(String name, Function<Item.Settings, T> factory, Item.Settings settings) {
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(YunosBosses.MOD_ID, name));
+        T item = factory.apply(settings.registryKey(key));
+        return Registry.register(Registries.ITEM, key, item);
     }
+
     public static void registerModItems() {
         YunosBosses.LOGGER.info("Registering Mod Items for " + YunosBosses.MOD_ID);
 
